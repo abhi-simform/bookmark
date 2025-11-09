5adeadcdb0792c2397a9e2c88a99d8553fdb47d3
Abhishek <abhishek.sawant@simformsolutions.com>
2025-11-09 14:51:23 +0530

    fix: update manifest link and filename for consistency in production environment

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       injectRegister: 'auto',
-      manifestFilename: process.env.NODE_ENV === 'production' ? '/bookmark/manifest.webmanifest' : '/manifest.webmanifest',
+      manifestFilename: 'manifest.webmanifest',
       includeAssets: [
         'favicon.ico',
         'favicon.svg', 
